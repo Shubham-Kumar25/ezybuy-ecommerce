1302ba12ec9f78bb13f1b116078bc92cdbe31f5d
Shubham Kumar <ishubhamjnv@gmail.com>
2024-05-12 21:57:14 +0530

    login with firebase

--- src/pages/registration/Login.jsx
+++ src/pages/registration/Login.jsx
@@ -1,9 +1,56 @@
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../firebase/FirebaseConfig";
+import myContext from "../../context/myContext";
+import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+
+  const signin = async () => {
+    setLoading(true);
+    try {
+      const result = await signInWithEmailAndPassword(auth, email, password);
+      console.log(result);
+      localStorage.setItem("user", JSON.stringify(result));
+      toast.success("Signin Successfully", {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      window.location.href = "/";
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast.error("Sigin Failed", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-screen ">
-      <div className="px-10 py-10 bg-gray-800  rounded-xl">
+      {loading && <Loader />}
+      <div className="px-10 py-10 bg-gray-800 rounded-xl">
         <div className="">
           <h1 className="mb-4 text-xl font-bold text-center text-white">
             Login
@@ -13,6 +60,8 @@ function Login() {
           <input
             type="email"
             name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Email"
           />
@@ -20,12 +69,17 @@ function Login() {
         <div>
           <input
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             className=" bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none"
             placeholder="Password"
           />
         </div>
         <div className="flex justify-center mb-3 ">
-          <button className="w-full px-2 py-2 font-bold text-black bg-yellow-500 rounded-lg ">
+          <button
+            onClick={signin}
+            className="w-full px-2 py-2 font-bold text-black bg-yellow-500 rounded-lg "
+          >
             Login
           </button>
         </div>
