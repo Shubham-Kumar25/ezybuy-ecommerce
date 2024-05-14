7d7374ed995a02703a7feabe1cf4ec8402049365
Shubham Kumar <ishubhamjnv@gmail.com>
2024-05-14 16:06:51 +0530

    show products in cart

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,11 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Layout from "../../components/layout/Layout";
 import myContext from "../../context/myContext";
 import Modal from "../../components/modal/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteFromCart } from "../../redux/CartSlice";
+import { toast } from "react-toastify";
 
 function Cart() {
   const context = useContext(myContext);
   const { mode } = context;
+
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart);
+
+  const [totalAmount, setTotalAmount] = useState(0);
+  useEffect(() => {
+    let temp = 0;
+    cartItems.forEach((item) => {
+      temp = temp + parseInt(item.price);
+    });
+    setTotalAmount(temp);
+  }, [cartItems]);
+
+  const shipping = parseInt(100);
+
+  let grandTotal;
+  if (cartItems.length > 0) {
+    grandTotal = shipping + totalAmount;
+  } else {
+    grandTotal = 0;
+  }
+
+  const deleteCart = (item) => {
+    dispatch(deleteFromCart(item));
+    toast.success("Deleted Successfully");
+  };
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cartItems));
+  }, [cartItems]);
 
   return (
     <Layout>
@@ -17,59 +50,68 @@ function Cart() {
         }}
       >
         <h1 className="mb-10 text-2xl font-bold text-center">Cart Items</h1>
-        <div className="justify-center max-w-5xl px-6 mx-auto md:flex md:space-x-6 xl:px-0 ">
+        <div className="justify-center mb-[60%] max-w-5xl px-6 mx-auto md:flex md:space-x-6 xl:px-0 ">
           <div className="rounded-lg md:w-2/3 ">
-            <div
-              className="justify-between p-6 mb-6 bg-white border rounded-lg drop-shadow-xl sm:flex sm:justify-start"
-              style={{
-                backgroundColor: mode === "dark" ? "rgb(32 33 34)" : "",
-                color: mode === "dark" ? "white" : "",
-              }}
-            >
-              <img
-                src="https://dummyimage.com/400x400"
-                alt="product-image"
-                className="w-full rounded-lg sm:w-40"
-              />
-              <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                <div className="mt-5 sm:mt-0">
-                  <h2
-                    className="text-lg font-bold text-gray-900"
-                    style={{ color: mode === "dark" ? "white" : "" }}
-                  >
-                    This is title
-                  </h2>
-                  <h2
-                    className="text-sm text-gray-900"
-                    style={{ color: mode === "dark" ? "white" : "" }}
-                  >
-                    desc
-                  </h2>
-                  <p
-                    className="mt-1 text-xs font-semibold text-gray-700"
-                    style={{ color: mode === "dark" ? "white" : "" }}
-                  >
-                    ₹100
-                  </p>
+            {cartItems.map((item, index) => {
+              const { title, imageUrl, description, category, price } = item;
+              return (
+                <div
+                  key={index}
+                  className="justify-between p-6 mb-6 bg-white border rounded-lg drop-shadow-xl sm:flex sm:justify-start"
+                  style={{
+                    backgroundColor: mode === "dark" ? "rgb(32 33 34)" : "",
+                    color: mode === "dark" ? "white" : "",
+                  }}
+                >
+                  <img
+                    src={imageUrl}
+                    alt={title}
+                    className="w-full rounded-lg sm:w-40"
+                  />
+                  <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
+                    <div className="mt-5 sm:mt-0">
+                      <h2
+                        className="text-lg font-bold text-gray-900"
+                        style={{ color: mode === "dark" ? "white" : "" }}
+                      >
+                        {title}
+                      </h2>
+                      <h2
+                        className="text-sm text-gray-900"
+                        style={{ color: mode === "dark" ? "white" : "" }}
+                      >
+                        {description}
+                      </h2>
+                      <p
+                        className="mt-1 text-xs font-semibold text-gray-700"
+                        style={{ color: mode === "dark" ? "white" : "" }}
+                      >
+                        ₹{price}
+                      </p>
+                    </div>
+                    <div
+                      onClick={() => deleteCart(item)}
+                      className="flex justify-between mt-4 cursor-pointer sm:space-y-6 sm:mt-0 sm:block sm:space-x-6"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                        />
+                      </svg>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex justify-between mt-4 sm:space-y-6 sm:mt-0 sm:block sm:space-x-6">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-6 h-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
 
           <div
@@ -90,7 +132,7 @@ function Cart() {
                 className="text-gray-700"
                 style={{ color: mode === "dark" ? "white" : "" }}
               >
-                ₹100
+                {totalAmount}
               </p>
             </div>
             <div className="flex justify-between">
@@ -104,7 +146,7 @@ function Cart() {
                 className="text-gray-700"
                 style={{ color: mode === "dark" ? "white" : "" }}
               >
-                ₹20
+                {cartItems.length > 0 ? shipping : 0}
               </p>
             </div>
             <hr className="my-4" />
@@ -120,7 +162,7 @@ function Cart() {
                   className="mb-1 text-lg font-bold"
                   style={{ color: mode === "dark" ? "white" : "" }}
                 >
-                  ₹200
+                  {grandTotal}
                 </p>
               </div>
             </div>
